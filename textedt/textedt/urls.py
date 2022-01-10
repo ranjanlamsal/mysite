abe9b71ef8c4f3a190569c5c5457dae2a275c5ca
Ranjan <lamsalranjana19@gmail.com>
2022-01-10 21:29:33 +0545

    added files to remote repo

---
+++ textedt/textedt/urls.py
@@ -0,0 +1,31 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
+    path('signup', views.signup, name='signup'),
+    path('login', views.login, name='login'),
+    path('thanks', views.thanks, name='thanks'),
+    path('welcome', views.welcome, name='welcome'),
+    path('analyzed', views.analyzed, name='analyzed'),
+    path('textedit', views.textedit, name='textedit'),
+]
